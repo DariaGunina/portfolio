661d6f7d3ac5e4bc127b1745c0fd9ed485795235
Daria <dar.raewa@yandex.ru>
2023-02-24 22:37:32 +0300

    Add task 6

--- src/features/StepsSection/StepsSection.tsx
+++ src/features/StepsSection/StepsSection.tsx
@@ -1,7 +1,9 @@
 import Image from "next/image";
 import { Title } from "../../components/Title";
 import { Step } from "../Step";
-import ImageCamera from "../../assets/camera.jpg";
+import ImageCamera1 from "../../assets/camera.jpg";
+import ImageCamera2 from "../../assets/camera2.jpg";
+import ImageCamera3 from "../../assets/camera3.jpg";
 import { IInformation } from "../../interface";
 
 import styles from "./StepsSection.module.css";
@@ -17,22 +19,23 @@ export const StepsSection = ({ information }: Props) => {
     <div className={styles.container}>
       <Title name="Как происходит процесс съемки?" />
       <div className={styles.section}>
-        <div className={styles.box}>
-          <div className={styles.image}>
-            <Image src={ImageCamera} alt="image" width={500} height={380} />
+        <div className={styles.images}>
+          <div className={styles.image1}>
+            <Image src={ImageCamera1} alt="image" width={500} height={380} />
           </div>
-          <div className={styles.text}>
-            {steps.map((item, i) => {
-              return (
-                <Step
-                  step={i + 1}
-                  title={item.title}
-                  text={item.text}
-                  key={i}
-                />
-              );
-            })}
+          <div className={styles.image2}>
+            <Image src={ImageCamera2} alt="image" width={500} height={380} />
           </div>
+          <div className={styles.image3}>
+            <Image src={ImageCamera3} alt="image" width={500} height={380} />
+          </div>
+        </div>
+        <div className={styles.text}>
+          {steps.map((item, i) => {
+            return (
+              <Step step={i + 1} title={item.title} text={item.text} key={i} />
+            );
+          })}
         </div>
       </div>
     </div>
